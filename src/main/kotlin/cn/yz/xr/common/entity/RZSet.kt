07cc2cc65a025f071ec4569768e50e1226a22d42
匡俊钢 <kuangjungang@foxmail.com>
2020-12-10 00:37:31 +0800

    add some function of RZSet

--- src/main/kotlin/cn/yz/xr/common/entity/RZSet.kt
+++ src/main/kotlin/cn/yz/xr/common/entity/RZSet.kt
@@ -2,6 +2,7 @@ package cn.yz.xr.common.entity
 
 import cn.yz.xr.common.utils.MessageUtil
 import io.netty.handler.codec.redis.ErrorRedisMessage
+import io.netty.handler.codec.redis.IntegerRedisMessage
 import io.netty.handler.codec.redis.RedisMessage
 import io.netty.handler.codec.redis.SimpleStringRedisMessage
 import java.lang.Exception
@@ -15,11 +16,11 @@ class RNode(var score:Int, var member:String):Comparable<RNode>{
 
 class RZSet(
         var zSet: LinkedHashMap<String,TreeSet<RNode>> = linkedMapOf(),
-        var operationList: List<String> = listOf("ZADD")
+        var operationList: List<String> = listOf("ZADD","ZSCORE","ZINCRBY","ZCARD","ZCOUNT","ZRANGE","ZRANK","ZREM")
 ) {
     private val comparator = kotlin.Comparator{ node1:RNode, node2:RNode->node2.score-node1.score}
 
-    fun zAdd(array: List<String>):RedisMessage{
+    fun ZAdd(array: List<String>):RedisMessage{
         var count = 0
         if(!zSet.containsKey(array[1])){
             this.zSet[array[1]] = sortedSetOf<RNode>(comparator)
@@ -31,7 +32,7 @@ class RZSet(
         return SimpleStringRedisMessage(count.toString())
     }
 
-    fun zScore(array: List<String>):RedisMessage{
+    fun ZScore(array: List<String>):RedisMessage{
         // 参数数目校验
         val paramError: ErrorRedisMessage? = MessageUtil.checkArgsNum(array, 3)
 
@@ -70,7 +71,7 @@ class RZSet(
         }
     }
 
-    fun zIncrBy(array: List<String>):RedisMessage{
+    fun ZIncrBy(array: List<String>):RedisMessage{
         // 参数数目校验
         val paramError: ErrorRedisMessage? = MessageUtil.checkArgsNum(array, 4)
 
@@ -103,8 +104,131 @@ class RZSet(
         }
     }
 
+    fun ZCard(array: List<String>):RedisMessage{
+
+        // 参数数目校验
+        val paramError: ErrorRedisMessage? = MessageUtil.checkArgsNum(array, 2)
+
+        return if(paramError==null){
+            if(this.zSet.containsKey(array[1])){
+                val length:Long = this.zSet[array[1]]?.size?.toLong()!!
+                IntegerRedisMessage(length)
+            }else{
+                IntegerRedisMessage(0L)
+            }
+        }else{
+            paramError
+        }
+    }
+
+    fun ZCount(array: List<String>):RedisMessage{
+        // 参数数目校验
+        val paramError: ErrorRedisMessage? = MessageUtil.checkArgsNum(array, 4)
+
+        return if(paramError==null){
+            if(this.zSet.containsKey(array[1])){
+                val min = array[2].toInt()
+                val max = array[3].toInt()
+                var count = 0L
+                this.zSet[array[1]]?.forEach { it-> if (it.score < max + 1 && it.score > min - 1){count+=1} }
+                IntegerRedisMessage(count)
+            }else{
+                IntegerRedisMessage(0L)
+            }
+        }else{
+            paramError
+        }
+    }
+
+    fun ZRange(array: List<String>):RedisMessage{
+        return if(array.size in listOf(4,5)){
+            var start = array[2].toInt()
+            var end = array[3].toInt()
+            val hasScore = array.size==5
+            if(this.zSet.containsKey(array[0])){
+                if(start > end || start > this.zSet[array[0]]?.size ?:0 ){
+                    SimpleStringRedisMessage("(empty list or set)")
+                }else{
+                    if(start < 0){
+                        start += this.zSet[array[0]]?.size ?: 0
+                        if(start < 0){
+                            SimpleStringRedisMessage("(empty list or set)")
+                        }
+                    }
+                    if(end < 0){
+                        end += this.zSet[array[0]]?.size ?: 0
+                        if(end < 0){
+                            SimpleStringRedisMessage("(empty list or set)")
+                        }
+                    }
+                    var index = 0
+                    var lineIndex = 1
+                    var output = ""
+                    for(i in this.zSet[array[0]]!!){
+                        if(index in start..end){
+                            output = "${lineIndex}) ${i.member}\n"
+                            lineIndex+=1
+                            if(hasScore){
+                                output = "${lineIndex}) ${i.score}\n"
+                                lineIndex+=1
+                            }
+                        }
+                        index+=1
+                    }
+                    SimpleStringRedisMessage(output)
+                }
+            }else{
+                SimpleStringRedisMessage("(empty list or set)")
+            }
+        }else{
+            ErrorRedisMessage("wrong number of arguments (given ${array.size}, expected 3)")
+        }
+    }
+
+    fun ZRank(array: List<String>):RedisMessage{
+        // 参数数目校验
+        val paramError: ErrorRedisMessage? = MessageUtil.checkArgsNum(array, 3)
+
+        return if(paramError==null){
+            if(this.zSet.containsKey(array[1]) && this.zSet[array[1]]?.size ?: -1 > 0){
+                var count = 0
+                for(node in this.zSet[array[1]]!!){
+                    if(node.member==array[2]){
+                        IntegerRedisMessage(count.toLong())
+                    }
+                    count+=1
+                }
+            }
+            SimpleStringRedisMessage("(nil)")
+        }else{
+            paramError
+        }
+    }
+
+    fun ZREM(array: List<String>):RedisMessage{
+        return if(array.size > 2){
+            var count = 0L
+            for(i in 2 until array.size){
+                if(this.zSet.remove(array[i])!=null){
+                    count+=1
+                }
+            }
+            IntegerRedisMessage(count)
+        }else{
+           ErrorRedisMessage("ERR wrong number of arguments for 'zrem' command")
+        }
+    }
+
     fun operation(command: String, array: List<String>): RedisMessage {
         return when(command){
+            "ZADD"->ZAdd(array)
+            "ZSCORE"->ZScore(array)
+            "ZINCRBY"->ZIncrBy(array)
+            "ZCARD"->ZCard(array)
+            "ZCOUNT"->ZCount(array)
+            "ZRANGE"->ZRange(array)
+            "ZRANK"->ZRank(array)
+            "ZREM"->ZREM(array)
             else -> ErrorRedisMessage("not supported command")
         }
     }
